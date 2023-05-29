d4ecc2018d0325ef6dd7811d90107a9289a4ad68
lingdianshiren <2077358365@qq.com>
2023-05-29 09:59:12 +0800

    全部完成

--- main.py
+++ main.py
@@ -1,147 +1,89 @@
+import ctypes
 import sys
 
 import networkx as nx
 import numpy as np
-# from Mainwin import Ui_Form
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QSizePolicy
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+from Mainwin import Ui_Form
 
-class Ui_Form(object):
-
-    def __init__(self):
-        self.inputtext = None
-        self.outputLabel = None
-        self.graphCanvas = None
-        self.tipsLabel = None
-        self.pushButton = None
-        self.centralwidget = None
-
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(700, 700)
-        Form.setMinimumSize(700, 700)
-        Form.setMaximumSize(700, 700)
-        self.pushButton = QtWidgets.QPushButton(Form, text="是欧拉图吗？")
-        self.pushButton.setGeometry(QtCore.QRect(450, 270, 99, 28))
-        self.pushButton.setIconSize(QtCore.QSize(20, 20))
-        self.pushButton.setObjectName("pushButton")
-        self.tipsLabel = QtWidgets.QLabel(Form)
-        self.tipsLabel.setGeometry(QtCore.QRect(350, 30, 291, 191))
-        self.tipsLabel.setStyleSheet("font: 75 9pt \"微软雅黑\";")
-        self.tipsLabel.setObjectName("tipsLabel")
-        self.graphCanvas = FigureCanvas(Figure(figsize=(3, 3)))
-        self.graphCanvas.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.graphCanvas.updateGeometry()
-        self.graphCanvas.setParent(Form)
-        self.graphCanvas.move(30, 360)
-        self.outputLabel = QtWidgets.QLabel(Form)
-        self.outputLabel.setGeometry(QtCore.QRect(350, 360, 300, 300))
-        self.outputLabel.setText("")
-        self.outputLabel.setObjectName("outputLabel")
-        self.inputtext = QtWidgets.QTextEdit(Form)
-        self.inputtext.setGeometry(QtCore.QRect(30, 30, 300, 300))
-        self.inputtext.setObjectName("inputtext")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-        Form.setTabOrder(self.inputtext, self.pushButton)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "isEulerGraph"))
-        self.pushButton.setText(_translate("Form", "是欧拉图吗？"))
-        self.tipsLabel.setText(_translate("Form", "<html><head/><body><p align=\"center\"><span style=\" "
-                                                  "font-size:11pt;\">请在左边的文本框中按行输入</span></p><p "
-                                                  "align=\"center\"><span style=\" "
-                                                  "font-size:11pt;\">一个表示图的邻接矩阵</span></p><p align=\"center\"><span "
-                                                  "style=\" font-size:11pt;\">格式要求：矩阵元素以英文逗号分割，</span></p><p "
-                                                  "align=\"center\"><span style=\" "
-                                                  "font-size:11pt;\">每行行末标记英文逗号</span></p><p align=\"center\"><span "
-                                                  "style=\" font-size:11pt;\">最后一行不写逗号</span></p></body></html>"))
-        self.inputtext.setHtml(_translate("Form",
-                                          "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" "
-                                          "\"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                          "<html><head><meta name=\"qrichtext\" content=\"1\" /><style "
-                                          "type=\"text/css\">\n"
-                                          "p, li { white-space: pre-wrap; }\n"
-                                          "</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; "
-                                          "font-weight:400; font-style:normal;\">\n"
-                                          "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                          "margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0,1,0,0,0;</p>\n"
-                                          "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                          "margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0,0,1,0,0;</p>\n"
-                                          "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                          "margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0,0,0,1,0;</p>\n"
-                                          "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                          "margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0,0,0,0,1;</p>\n"
-                                          "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; "
-                                          "margin-right:0px; -qt-block-indent:0; text-indent:0px;\">1,0,0,0,"
-                                          "0</p></body></html>"))
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
 
 
-class Example(QWidget):
+class Main(QWidget):
 
     def __init__(self):
         super().__init__()
         self.ui = None
         self.axes = None  # 保存绘图区域
+        self.graphType = ""
+        self.judgeEuler = True
+        self.G = None
         self.initUI()
 
     def initUI(self):
         self.setGeometry(300, 300, 700, 700)
         self.setWindowTitle('isEulerGraph')
-        self.setWindowIcon(QIcon('icon.png'))
+        self.setWindowIcon(QIcon('winico.ico'))
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.getGraph)
         self.ui.pushButton.clicked.connect(self.plotGraph)
+        self.ui.pushButton.clicked.connect(self.outJudge)
         self.show()
 
-    type = ""
+    def getGraph(self):
+        while True:
+            try:
+                matrix = np.matrix(self.ui.inputtext.toPlainText())
+                break
+            except:
+                QtWidgets.QMessageBox.warning(self, "错误", "输入格式错误，请按照要求重新输入！")
+                return
+        if (matrix == matrix.T).all():
+            self.graphType = "Undirected"
+            self.G = nx.Graph(matrix)
+            self.judgeEuler = all(d % 2 == 0 for v, d in self.G.degree()) and nx.is_connected(self.G)
+        else:
+            self.graphType = "Directed"
+            self.G = nx.DiGraph(matrix)
+            self.judgeEuler = all(
+                self.G.out_degree(n) == self.G.in_degree(n) for n in self.G) and nx.is_strongly_connected(self.G)
 
     def plotGraph(self):
         self.ui.graphCanvas.figure.clear()  # 清空绘图区域
-        if self.type == "Undirected":
-            G = nx.Graph()
-        elif self.type == "Directed":
-            G = nx.DiGraph()
-        G = self.getGraph()
         fig = Figure(figsize=(3, 3))
         ax = fig.add_subplot(111)
         ax.axis('off')
         ax.set_aspect('equal')
-        pos = nx.spring_layout(G, seed=42)  # 使用spring layout布局，seed保证每次绘制结果一致
-        nx.draw_networkx_nodes(G, pos, node_size=100, node_color='w', edgecolors='k', ax=ax)
-        if self.type == "Undirected":
-            nx.draw_networkx_edges(G, pos, width=1, ax=ax)
-        elif self.type == "Directed":
-            nx.draw_networkx_edges(G, pos, width=1, arrows=True, arrowstyle='->', arrowsize=5, ax=ax)
+        pos = nx.spring_layout(self.G, seed=42)  # 使用spring layout布局，seed保证每次绘制结果一致
+        nx.draw_networkx_nodes(self.G, pos, node_size=100, node_color='w', edgecolors='k', ax=ax)
+        if self.graphType == "Undirected":
+            nx.draw_networkx_edges(self.G, pos, width=1, ax=ax)
+        elif self.graphType == "Directed":
+            nx.draw_networkx_edges(self.G, pos, width=1, arrows=True, arrowstyle='->', arrowsize=5, ax=ax)
         self.ui.graphCanvas.setGeometry(QtCore.QRect(30, 360, 300, 300))
         self.ui.graphCanvas.figure = fig
         self.ui.graphCanvas.draw()
 
-    def getGraph(self):
-        matrix = np.matrix(self.ui.inputtext.toPlainText())
-        if (matrix == matrix.T).all():
-            self.type = "Undirected"
-            G = nx.Graph(matrix)
+    def outJudge(self):
+        outString = "您输入的"
+        if self.graphType == "Undirected":
+            outString = outString + "无向图"
         else:
-            self.type = "Directed"
-            G = nx.DiGraph(matrix)
-        return G
+            outString = outString + "有向图"
+        if self.judgeEuler:
+            outString = outString + "是"
+        else:
+            outString = outString + "不是"
+        outString = outString + "欧拉图！"
+        self.ui.outputLabel.setText(outString)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    main = Main()
     sys.exit(app.exec_())
-
-"""
-0,1,1;
-1,0,1;
-1,1,0
-"""
--- test/tt.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-if __name__ == "__main__":
-    matrix = np.matrix([1])
-    print(matrix)
